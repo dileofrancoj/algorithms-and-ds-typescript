2e929791afc3511bfebcd475a596cb83e9f84fbe
Franco Di Leo <dileo.francoj@gmail.com>
2024-06-07 12:12:07 -0300

    chore: stack implementation

--- src/data-structures/stack.ts
+++ src/data-structures/stack.ts
@@ -1,3 +1,57 @@
-export class Stack {
-  public stack = []
+/*
+  Una pila / stack es una estructura de datos se basa en LiFo (Last in first out)
+
+  Definición de interfaz:
+  length: devuelve la cantidad de elementos que tiene la Pila
+  push: recibe un generico y agrega ese elemento al final del array
+  isEmpty: Devuelve un booleano dependiendo si la pila está vacía o no
+  top: Devuelve el último elemento del array
+  pop: Devuelve y elimina el úlitmo elemento de la pila
+
+*/
+interface StackProps<T> {
+  length: () => number
+  push: (value: T) => void
+  isEmpty: () => boolean
+  top: () => T | undefined
+  pop: () => T | never
+}
+
+/* Nota: se agregan comprobaciones respecto al tamaño en memoria que puede tener la pila */
+export class Stack<T> implements StackProps<T> {
+  private readonly stack: T[] = []
+  private readonly limit: number
+
+  constructor (limit: number = Number.MAX_SAFE_INTEGER) {
+    this.limit = limit
+  }
+
+  length (): number {
+    return this.stack.length
+  }
+
+  isEmpty (): boolean {
+    return this.length() === 0
+  }
+
+  push (value: T): void | never {
+    if (this.length() + 1 > this.limit) {
+      throw new Error('Stack overflow')
+    }
+    this.stack.push(value)
+  }
+
+  top (): T | undefined {
+    if (!this.isEmpty()) {
+      return this.stack.at(-1)
+    }
+    return undefined
+  }
+
+  pop (): T | never {
+    if (this.length() !== 0) {
+      return this.stack.pop() as T
+    }
+    throw new Error('Stack underflow')
+  }
 }
